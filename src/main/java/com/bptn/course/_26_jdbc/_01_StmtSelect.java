43a9f9bfa74d865e13a08b13101bd6242112b04a
Aemade <aemade.terefe@gmail.com>
2024-10-29 19:01:35 -0400

    JDBC

---
+++ src/main/java/com/bptn/course/_26_jdbc/_01_StmtSelect.java
@@ -0,0 +1,12 @@
+package com.bptn.course._26_jdbc;
+
+public class _01_StmtSelect {
+
+	public static void main(String[] args) {
+		String dbURL = "jdbc:postgresql://localhost:5432/bptn";
+		String dbUsername = "postgres";
+		String dbPassword = "Bebest12!";
+
+	}
+
+}
